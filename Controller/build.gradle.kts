f54be563d7e8c08f6e14d3e1121d8b9abca821e3
Sobenin Anton <toshkasob@users.noreply.github.com>
2021-10-13 00:40:16 +0300

    not everything showed up inthe last commit

--- Controller/build.gradle.kts
+++ Controller/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 dependencies {
     implementation(project(":Models"));
     implementation("com.google.code.gson:gson:2.8.8");
-    implementation("com.intellij:annotations:12.0")
+//    implementation("com.intellij:annotations:12.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
