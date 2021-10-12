0977e3ae3f9f2b92aa060033cc40998723e94a69
Sobenin Anton <toshkasob@users.noreply.github.com>
2021-10-13 00:36:36 +0300

    some changes in dependencies (but lombok can't import as well)
    ДЗ №1: Создание проектов в IntelliJ IDEA с помощью системы сборки Gradle.
    
    P.S. Manual upload in https://github.com/toshkasob/Gamesphere_Java-11 because no ssh
    
    
    IntelliJ IDEA 2021.2.2 (Community Edition)
    Build #IC-212.5284.40, built on September 14, 2021
    Runtime version: 11.0.12+7-b1504.28 amd64
    VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
    Windows 7 6.1
    GC: G1 Young Generation, G1 Old Generation
    Memory: 1022M
    Cores: 8
    Non-Bundled Plugins: org.jetbrains.kotlin (212-1.5.31-release-546-IJ4638.7)
    Kotlin: 212-1.5.31-release-546-IJ4638.7

--- Models/build.gradle.kts
+++ Models/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.intellij:annotations:12.0")
+//    implementation("com.intellij:annotations:12.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
